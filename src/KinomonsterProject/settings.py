c7d141ed68dcf683efd3836ca60d084cdfe1bf81
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-07-23 12:32:40 +0300

    Update settings.py

--- src/KinomonsterProject/settings.py
+++ src/KinomonsterProject/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'KinomonsterProject.wsgi.application'
 
 #Database
 #https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'KinomonsterDatabase',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        #'HOST': 'localhost',
-        'HOST': '213.139.209.173',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'KinomonsterDatabase',
+        # 'USER': 'admin',
+        # 'PASSWORD': 'admin',
+        # #'HOST': 'localhost',
+        # 'HOST': '213.139.209.173',
+        # 'PORT': '5432'
+    # }
+# }
 
 
 # Password validation

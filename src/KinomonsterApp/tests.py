afe7c3e016b5c05cb0698f9c00f8aac82fc51fda
garrip91 <garrip91@gmail.com>
2022-08-08 17:07:38 +0300

    Summary

--- src/KinomonsterApp/tests.py
+++ src/KinomonsterApp/tests.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 
 
 # Create your tests here.
-send_mail('NEW_TITLE_07.07.2021', 'NEW_MESSAGE_07.07.2021', 'garrip91@yandex.ru', ['garrip91@yandex.ru'], fail_silently=False)   ### Я ДОБАВИЛ
+#send_mail('NEW_TITLE_07.07.2021', 'NEW_MESSAGE_07.07.2021', 'garrip91@yandex.ru', ['garrip91@yandex.ru'], fail_silently=False)   ### Я ДОБАВИЛ
--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,25 +1,39 @@
 from django.test import TestCase
 
+from KinomonsterApp.models import Film
 
-class ModelTestClass(TestCase):
+
+class FilmModelTestClass(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData: запустите один раз, чтобы настроить немодифицированные данные для всех методов класса")
+        # Настраиваем немодифицированные объекты, используемые всеми методами тестирования:
+        Film.objects.create(title='ЗАГОЛОВОК ДЛЯ ТЕСТА', logo='images/ДЛЯ_ТЕСТА.png', youtube_trailer_url='https://www.youtube.com/watch?v=FquXN0t_KbU', year='2022', rating='666666', producer='РЕЖИССЁР ДЛЯ ТЕСТА', description='ОПИСАНИЕ ДЛЯ ТЕСТА')
+
+    def test_title_label(self):
+        film = Film.objects.get(id=1)
+        field_label = film._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'Название фильма')
+
+    def test_logo_label(self):
+        film = Film.objects.get(id=1)
+        field_label = film._meta.get_field('logo').verbose_name
+        self.assertEquals(field_label, 'Логотип фильма')
+
+    def test_youtube_trailer_url_max_length(self):
+        film = Film.objects.get(id=1)
+        max_length = film._meta.get_field('youtube_trailer_url').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_ABC_DEF_1(self):
         pass
 
-    def setUp(self):
-        print("setUp: запустите один раз для каждого метода тестирования, чтобы настроить чистые данные")
+    def test_ABC_DEF_2(self):
         pass
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false")
-        self.assertFalse(False)
+    def test_ABC_DEF_3(self):
+        pass
 
-    def test_false_is_true(self):
-        print("Method: test_false_is_true")
-        self.assertTrue(False)
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two")
-        self.assertEqual(1 + 1, 2)
+    def test_get_absolute_url(self):
+        film = Film.objects.get(id=1)
+        self.assertEquals(film.get_absolute_url(), '/catalog/author/1')
